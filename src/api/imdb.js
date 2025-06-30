1cda22d6bb099607f467d66c88b14d93f7e81305
Sandip-xD <140407921+Sandipeyy@users.noreply.github.com>
2025-06-30 08:07:08 +0545

    Update imdb.js

--- src/api/imdb.js
+++ src/api/imdb.js
@@ -1,29 +1,12 @@
+// src/api/imdb.js
 import axios from "axios";
 
 const API_KEY = "76d6bb3cd65280309281489c42bc5dca";
 const BASE_URL = "https://imdb-api.com/en/API";
 
-export const getTrendingMovies = async () => {
+export const getMovieDetails = async (imdbId) => {
   try {
-    const response = await axios.get(`${BASE_URL}/Top250Movies/${API_KEY}`);
-    return response.data.items;
-  } catch (error) {
-    console.error("Error fetching trending movies:", error);
-  }
-};
-
-export const getTrendingTVShows = async () => {
-  try {
-    const response = await axios.get(`${BASE_URL}/Top250TVs/${API_KEY}`);
-    return response.data.items;
-  } catch (error) {
-    console.error("Error fetching trending TV shows:", error);
-  }
-};
-
-export const getMovieDetails = async (id) => {
-  try {
-    const response = await axios.get(`${BASE_URL}/Title/${API_KEY}/${id}`);
+    const response = await axios.get(`${BASE_URL}/Title/${API_KEY}/${imdbId}`);
     return response.data;
   } catch (error) {
     console.error("Error fetching movie details:", error);
